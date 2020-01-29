d46bd12cb07e13d3a69a4dbf13003c0e79407839
matthewbarondeau <mebarondeau@utexas.edu>
2020-01-29 13:49:25 -0600

    Add spo2 and heartrate calculation for MAX30102

---
+++ Embedded Systems and Firmware/MAX30102_Test_Project/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../heartRate.c \
+../main.c \
+../spo2.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./heartRate.d \
+./main.d \
+./spo2.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./heartRate.obj \
+./main.obj \
+./spo2.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"heartRate.obj" \
+"main.obj" \
+"spo2.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"heartRate.d" \
+"main.d" \
+"spo2.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../heartRate.c" \
+"../main.c" \
+"../spo2.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
