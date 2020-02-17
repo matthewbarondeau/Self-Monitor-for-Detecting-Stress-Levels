6efb52b40c70fd79a39a55545e328c6c6c5c09f9
matthewbarondeau <mebarondeau@utexas.edu>
2020-02-17 10:05:51 -0600

    Remove i2c register routines from accelerometer file

--- Embedded Systems and Firmware/Self Monitor Stress System/Accelerometer.c
+++ Embedded Systems and Firmware/Self Monitor Stress System/Accelerometer.c
@@ -11,16 +11,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
-#include "driverlib.h"
-//#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-
+#include "driverlib/MSP432P4xx/driverlib.h"
 /* Slave Address for I2C Slave */
 #define SLAVE_ADDRESS       0x1D
 #define NUM_OF_REC_BYTES    32
 
 /* Variables */
 const uint8_t TXData[] = {0x41,0x42};
-static uint8_t RXData;
 
 int16_t x;
 int16_t y;
@@ -36,41 +33,6 @@ const eUSCI_I2C_MasterConfig i2cConfig =
         0,                                      // No byte counter threshold
         EUSCI_B_I2C_NO_AUTO_STOP                // No Autostop
 };
-
-void writeRegister8(uint8_t reg, uint8_t value){
-    //while(MAP_I2C_masterIsStartSent(EUSCI_B0_BASE));
-    MAP_I2C_masterSendMultiByteStart(EUSCI_B0_BASE, reg);  // Start + 1Byte
-    MAP_I2C_masterSendMultiByteNext(EUSCI_B0_BASE, value); // Poll for TXINT,Send 1Byte
-
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
-    while (MAP_I2C_masterIsStopSent(EUSCI_B0_BASE));
-
-}
-
-uint8_t readRegister8(uint8_t reg){
-
-    /* Making sure the last transaction has been completely sent out */
-    while (MAP_I2C_masterIsStopSent(EUSCI_B0_BASE));
-
-    /* Send out EEPROM Mock Read Cmd (2 databytes) */
-    MAP_I2C_masterSendMultiByteStart(EUSCI_B0_BASE, reg);  // Start + 1Byte
-    //MAP_I2C_masterSendMultiByteNext(EUSCI_B0_BASE, TXData[0]); // Poll for TXINT,Send 1Byte
-    /*---------------------------------------------*/
-    /* Now we need to initiate the read */
-    /* Wait until 2nd Byte has been output to shift register */
-    while(!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0));
-
-    // Send the restart condition, read one byte, send the stop condition right away
-    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_TR);
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;
-    while(MAP_I2C_masterIsStartSent(EUSCI_B0_BASE));
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
-    while(!(EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0));
-    RXData = EUSCI_B0->RXBUF;
-
-    return RXData;
-}
-
 
 void Accelerometer_Init(){
     /* Initializing I2C Master to SMCLK at 100khz with no autostop */
