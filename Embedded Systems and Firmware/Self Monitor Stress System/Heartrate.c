f8c1563b375f90d1ec4755669153c38036cdd595
matthewbarondeau <mebarondeau@utexas.edu>
2020-02-17 10:07:37 -0600

    Add extra functions for heart rate and Sp02 retreival

---
+++ Embedded Systems and Firmware/Self Monitor Stress System/Heartrate.c
@@ -0,0 +1,8 @@
+/*
+ * Heartrate.c
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: Matthew Barondeau
+ */
+
+
--- Embedded Systems and Firmware/Self Monitor Stress System/ROS.c
+++ Embedded Systems and Firmware/Self Monitor Stress System/ROS.c
@@ -8,12 +8,23 @@
 #include "msp.h"
 #include <stdint.h>
 
+// MAX30102 Includes
+#include "SpO2.h"
+#include "Heartrate.h"
+#include "MAX30102.h"
 
 void ROS_Init(){
-
-
+    MAX30102_Init();
 }
 
 int8_t ROS_Read_Data(){
+    return 0;
+}
+
+int8_t ROS_Read_Heartrate(){
+    return 0;
+}
+
+int8_t ROS_Read_SpO2(){
     return 0;
 }
