9e313d7b6cb1c063da7bc2e23cf959929d97ff13
matthewbarondeau <mebarondeau@utexas.edu>
2020-05-21 11:28:39 -0500

    Finish cleaning up bluetooth sensor test project

--- Embedded Systems and Firmware/Sensor_Test_Projects/Bluetooth_Sensor/main.c
+++ Embedded Systems and Firmware/Sensor_Test_Projects/Bluetooth_Sensor/main.c
@@ -1,64 +1,50 @@
 
-//------------See AP.c for details of hardware connections to CC2650--------------------
-//------------See LaunchPad.c for details of switches and LEDs--------------------------
-
 #include <stdint.h>
+#include "msp.h"
+#include "Drivers/AP.h"
+#include "Drivers/adc.h"
+#include "Drivers/UART0.h"
 #include "Drivers/Clock.h"
 #include "Drivers/CortexM.h"
-#include "Drivers/AP.h"
-#include "Drivers/UART0.h"
-#include "Drivers/LaunchPad.h"
-#include "msp.h"
-#include "adc.h"
 
-uint8_t LEDs;
+// Global ADC value
+uint16_t ADC;
 
-// ********OutValue**********
-// Debugging dump of a data value to virtual serial port to PC
-// data shown as 1 to 8 hexadecimal characters
-// Inputs:  response (number returned by last AP call)
-// Outputs: none
-void OutValue(char *label,uint32_t value){ 
-  UART0_OutString(label);
-  UART0_OutUHex(value);
-}
-
-void readLED(void){
-    OutValue("\n\rRead LED=", LEDs);
-}
-
-void writeLED(void){
-    LaunchPad_Output(LEDs);
-    OutValue("\n\rWrote LED= ", LEDs);
+void ADC6(void){
+    UART0_OutString("ADC: ");
+    UART0_OutUDec(ADC);
+    UART0_OutString("\n\r");
 }
 
 int main(void){
     volatile int r;
     int time=0;
     DisableInterrupts();
+
+    // Initialize Clock to 48 MHz
     Clock_Init48MHz();
+
+    // Initialize UART0 for debug
     UART0_Init();
-    LaunchPad_Init();  // input from switches, output to LEDs on LaunchPad
+
+    // Initialize ADC
     ADC0_Ch6_Init();
+
     EnableInterrupts();
-    UART0_OutString("\n\rApplication Processor - MSP432-CC2650\n\r");
+
     // Initialize Bluetooth
     r = AP_Init();
     AP_GetStatus();  // optional
     AP_GetVersion(); // optional
     AP_AddService(0xFFF0);
 
-    // Example Characteristic
-    LEDs = P2->OUT & 0x7;
-    AP_AddCharacteristic(
-        0xFFF4,             // Unique ID Number
-        1,                  // Number of bytes to send
-        &LEDs,              // Pointer to the user data
-        0x03,               // Permission: 0=none, 1=Read, 2=Write, 3=Read+Write
-        0x0A,               // Properties: 2=Read, 8=Write, 0xA=Read+Write
-        "LEDs",             // Null-terminated string up to 20 bytes
-        &readLED,      // Read function called
-        &writeLED      // Write function called
+    // Add ADC notify characteristic
+    AP_AddNotifyCharacteristic(
+            0xFFF1,         // Characteristic number
+            2,              // Number of bytes the variable is
+            &ADC,           // Variable data that the bluetooth sends
+            "ADC0 Ch 6",    // Characteristic Name in application
+            &ADC6           // Function called send notification
     );
 
     //Start broadcasting Bluetooth
@@ -66,13 +52,21 @@ int main(void){
     AP_StartAdvertisement();
     AP_GetStatus(); // optional
 
-
     while(1){
+        // Time variable used to not overload the bluetooth module
         time++;
-        AP_BackgroundProcess();  // handle incoming SNP frames
-        int data = ADC_Ch6_Samples(128);
-        UART0_OutString("ADC: ");
-        UART0_OutUDec(data);
-        UART0_OutString("\n\r");
+
+        // handle incoming SNP frames
+        AP_BackgroundProcess();
+
+        // Send updated ADC value
+        if(time >2000000){
+            // Average 10 ADC samples
+            ADC = ADC_Ch6_Samples(10);
+            time = 0;
+            if(AP_GetNotifyCCCD(0)){
+                AP_SendNotification(0);
+            }
+        }
     }
 }
